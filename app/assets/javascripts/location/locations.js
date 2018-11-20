e4bf247600a43634c9af2962cadd32d411c48dc1
Dmytro Khelemendyk <dkhelemendyk@berkeley.edu>
2018-11-19 23:09:29 -0800

    add geocoder library and autocompletion in the search bar

--- app/assets/javascripts/location/locations.js
+++ app/assets/javascripts/location/locations.js
@@ -1,16 +1,16 @@
 //= require jquery
+var map, searchBox, geocoder;
 
 function initMap() {
-    var map;
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 37.77, lng: -122.43}, zoom: 12, disableDoubleClickZoom: true
     });
     infoWindow = new google.maps.InfoWindow;
 
     //add search box to the map
-    var searchInput = document.getElementById('pac-input');
-    var searchBtn = document.getElementById('search-button');
-    var searchBox = new google.maps.places.SearchBox(searchInput);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(document.getElementById('pac-input'));
+    searchBox = new google.maps.places.SearchBox(document.getElementById('pac-input'));
+    google.maps.event.addListener(searchBox, 'places_changed', processPlacesSearch);
 
     // find user's geolocation, center map on it
     if (navigator.geolocation) {
@@ -32,11 +32,55 @@ function initMap() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
 
-    map.addListener('dblclick', function(e) {
-    placeMarkerAndPanTo(e.latLng, map);
-    });
-
     var marker = new google.maps.Marker({position: {lat: 37.77, lng: -122.43}, map: map});
+
+    //Listen for the event fired when the user selects an item from the
+    //pick list. Retrieve the matching places for that item.
+    // google.maps.event.addListener(searchBox, 'places_changed', function() {
+    //     var places = searchBox.getPlaces();
+    //
+    //     if (places.length == 0) {
+    //       return;
+    //     }
+    //     for (var i = 0, marker; marker = markers[i]; i++) {
+    //       marker.setMap(null);
+    //     }
+    //
+    //     // For each place, get the icon, place name, and location.
+    //     markers = [];
+    //     var bounds = new google.maps.LatLngBounds();
+    //     for (var i = 0, place; place = places[i]; i++) {
+    //       var image = {
+    //         url: place.icon,
+    //         size: new google.maps.Size(71, 71),
+    //         origin: new google.maps.Point(0, 0),
+    //         anchor: new google.maps.Point(17, 34),
+    //         scaledSize: new google.maps.Size(25, 25)
+    //       };
+    //
+    //       // Create a marker for each place.
+    //       var marker = new google.maps.Marker({
+    //         map: map,
+    //         icon: image,
+    //         title: place.name,
+    //         position: place.geometry.location
+    //       });
+    //
+    //       markers.push(marker);
+    //
+    //       bounds.extend(place.geometry.location);
+    //     }
+    //
+    //     map.fitBounds(bounds);
+    //   });
+    //   // [END region_getplaces]
+    //
+    //   // Bias the SearchBox results towards places that are within the bounds of the
+    //   // current map's viewport.
+    //   google.maps.event.addListener(map, 'bounds_changed', function() {
+    //     var bounds = map.getBounds();
+    //     searchBox.setBounds(bounds);
+    //   });
   }
 
   function placeMarkerAndPanTo(latLng, map) {
@@ -45,6 +89,25 @@ function initMap() {
       map: map
     });
     map.panTo(latLng);
+  }
+
+  function processPlacesSearch() {
+    var places = searchBox.getPlaces();
+    if (places.length) {
+      location = places[0].geometry.location;
+      var origin = new google.maps.LatLng(location.lat, location.lng);
+      // plot origin
+    }
+  }
+
+  function processButtonSearch(location) {
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode(location, function (data) {
+      var lat = data[0].geometry.location.lat();
+      var lng = data[0].geometry.location.lng();
+      var origin = new google.maps.LatLng(lat, lng);
+      // plot origin
+    });
   }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
