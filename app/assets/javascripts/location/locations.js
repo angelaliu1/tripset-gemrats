15de89f5635b0b789108381e0abd1e51f81a34e0
Dmytro Khelemendyk <dkhelemendyk@berkeley.edu>
2018-11-19 22:09:53 -0800

    add marker on double click, disable zooming on double click, add search bar

--- app/assets/javascripts/location/locations.js
+++ app/assets/javascripts/location/locations.js
@@ -3,11 +3,16 @@
 function initMap() {
     var map;
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 37.77, lng: -122.43}, zoom: 12
+      center: {lat: 37.77, lng: -122.43}, zoom: 12, disableDoubleClickZoom: true
     });
     infoWindow = new google.maps.InfoWindow;
 
-    // Try HTML5 geolocation.
+    //add search box to the map
+    var searchInput = document.getElementById('pac-input');
+    var searchBtn = document.getElementById('search-button');
+    var searchBox = new google.maps.places.SearchBox(searchInput);
+
+    // find user's geolocation, center map on it
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
@@ -26,7 +31,20 @@ function initMap() {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
+
+    map.addListener('dblclick', function(e) {
+    placeMarkerAndPanTo(e.latLng, map);
+    });
+
     var marker = new google.maps.Marker({position: {lat: 37.77, lng: -122.43}, map: map});
+  }
+
+  function placeMarkerAndPanTo(latLng, map) {
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+    map.panTo(latLng);
   }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
