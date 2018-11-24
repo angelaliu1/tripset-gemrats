c80998057591ae261f3d0f86861e5335862df5c3
Dmytro Khelemendyk <dkhelemendyk@berkeley.edu>
2018-11-24 06:14:40 -0800

    location save clean code

--- app/assets/javascripts/location/locations.js
+++ app/assets/javascripts/location/locations.js
@@ -12,47 +12,32 @@ function initMap() {
 
     //add search box to the map
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(document.getElementById('pac-input'));
-    // searchBox = new google.maps.places.SearchBox(document.getElementById('pac-input'));
-    // google.maps.event.addListener(searchBox, 'places_changed', processPlacesSearch);
-
-
-
-
-
     var defaultBounds = new google.maps.LatLngBounds();
-                var options = {
-                bounds: defaultBounds
-                };
+    var options = {bounds: defaultBounds};
     var input = document.getElementById('pac-input');
     var autocomplete  = new google.maps.places.Autocomplete(input, options);
     autocomplete.addListener('place_changed', function() {
-                    var bounds = new google.maps.LatLngBounds();
-                    // Get place info
-                    var place = autocomplete.getPlace();
-                    // Do whatever with the value!
-                    console.log(place.geometry.location.lat());
-                    document.getElementById("location_name").value = place.name;
-                    document.getElementById("location_latitude").value = place.geometry.location.lat();
-                    document.getElementById("location_longitude").value = place.geometry.location.lng();
+          var bounds = new google.maps.LatLngBounds();
+          // Get location's info
+          var place = autocomplete.getPlace();
 
-                    var marker = new google.maps.Marker({
-                      map: map,
-                      title: place.name,
-                      position: place.geometry.location
-                    });
-                    // Create a marker for each place.
-                    markers.push(marker);
+          document.getElementById("location_name").value = place.name;
+          document.getElementById("location_latitude").value = place.geometry.location.lat();
+          document.getElementById("location_longitude").value = place.geometry.location.lng();
 
-                    if (place.geometry.viewport) {
-                        bounds.union(place.geometry.viewport);
-                    } else {
-                        bounds.extend(place.geometry.location);
-                    }
-                    map.fitBounds(bounds);
+          var marker = new google.maps.Marker({
+                map: map,
+                title: place.name,
+                position: place.geometry.location
                 });
 
-
-
+          if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+          } else {
+                bounds.extend(place.geometry.location);
+          }
+          map.fitBounds(bounds);
+    });
 
     // find user's geolocation, center map on it
     if (navigator.geolocation) {
@@ -79,48 +64,6 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
-  }
-
-  var markers = [];
-
-  function processPlacesSearch() {
-      var places = searchBox.getPlaces();
-      if (places.length == 0) {
-      return;
-      }
-      // Clear out the old markers.
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-
-      var bounds = new google.maps.LatLngBounds();
-      places.forEach(function(place) {
-          if (!place.geometry) {
-            console.log("Returned place contains no geometry");
-            return;
-          }
-
-          var marker = new google.maps.Marker({
-            map: map,
-            title: place.name,
-            position: place.geometry.location
-          });
-          // Create a marker for each place.
-          markers.push(marker);
-
-          google.maps.event.addListener(marker, 'click', function() {
-              // document.getElementById("location_name").value = place.name;
-              // document.getElementById("location_latitude").value = place.geometry.location.lat();
-              // document.getElementById("location_longitude").value = place.geometry.location.lng();
-            });
-
-          if (place.geometry.viewport) {
-              bounds.union(place.geometry.viewport);
-          } else {
-              bounds.extend(place.geometry.location);
-          }
-        });
-      map.fitBounds(bounds);
   }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
