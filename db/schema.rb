535fa4125400d06c567c2ca4631f71e3678a8975
Dmytro Khelemendyk <dkhelemendyk@berkeley.edu>
2018-11-27 22:32:39 -0800

    add buttons to the application.html.erb; add button to locations view; change latitude and longitude datatype to floats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_072019) do
+ActiveRecord::Schema.define(version: 2018_11_28_062824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(version: 2018_11_22_072019) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
-    t.decimal "latitude"
-    t.decimal "longitude"
+    t.float "latitude"
+    t.float "longitude"
     t.bigint "review_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
