3b2b3db94a5ee4eb94990e98ed813e5e82307833
Candice Ye <candiceye@berkeley.edu>
2018-11-28 14:38:34 -0800

    Form submit button now saves and redirects to home

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,12 @@
 class ReviewsController < ApplicationController
   def index
+  	@reviews = Review.all
   end
+
+  def create
+  	new_review = Review.create(title: params[:title], review: params[:review])
+  	new_review.save
+  	redirect_to root_path
+  end
+
 end
