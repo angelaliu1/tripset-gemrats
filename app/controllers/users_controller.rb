dbc9e59c8e6b4be8ec1ad8b5c357e59c3696980d
victorgrajamski <victor.grajski@gmail.com>
2018-11-28 18:10:52 -0800

    Merge branch 'vg_users' of https://github.com/dmitrokh/tripset-gemrats into vg_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,5 @@ class UsersController < ApplicationController
         @user = User.find(current_user.id)
         @reviews = Review.where(user_id: @user.id)
         @routes = Route.where(user_id: @user.id)
-        @meep = Route.all
     end
 end
